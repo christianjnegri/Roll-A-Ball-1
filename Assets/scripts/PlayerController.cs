3ece4df6a06ffe07e033b7e5386703c9bdc722e6
unknown <CJN49@CILABS-119A-10.scilsnet.rutgers.edu>
2016-02-04 21:00:50 -0500

    Added player controls

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Rigidbody
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement);
 	}
 }
